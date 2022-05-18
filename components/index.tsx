d1d7ebc15bf63aa1df362a45f1773cb88d7d1dd5
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-17 21:08:35 -0500

    added all components

---
+++ components/index.tsx
@@ -0,0 +1,27 @@
+export { default as Navbar } from '@/components/shared/Navbar';
+export { default as Section } from '@/components/shared/Section';
+export { default as Footer } from '@/components/shared/Footer';
+export { default as HeadOpenGraph } from '@/components/shared/HeadOpenGraph';
+export { default as BlurImage } from '@/components/shared/BlurImage';
+
+export { default as CTA } from '@/components/home/CTA';
+export { default as Location } from '@/components/home/Location';
+export { default as LocationSection } from '@/components/home/LocationSection';
+export { default as AboutUs } from '@/components/home/AboutUs';
+
+export { default as StoreRichTextRenderer } from '@/components/store/StoreRichTextRenderer';
+
+export { default as Application } from '@/components/careers/Application';
+export { default as JobPosting } from '@/components/careers/JobPosting';
+export { default as JobListingJSONLD } from '@/components/careers/JobListingJSONLD';
+export { default as JobListingRichTextRenderer } from '@/components/careers/JobListingRichTextRenderer';
+
+export { CustomInput as CustomInput } from '@/components/contact/FormInputs';
+export { TextFieldInput as TextFieldInput } from '@/components/contact/FormInputs';
+export { FormGroupLabel as FormGroupLabel } from '@/components/contact/FormInputs';
+export { RadioInput as RadioInput } from '@/components/contact/FormInputs';
+export { ErrorMessage as ErrorMessage } from '@/components/contact/FormInputs';
+
+export { default as PrivacyPolicy } from '@/components/legal/PrivacyPolicy';
+export { default as Terms } from '@/components/legal/Terms';
+export { default as AgreementToast } from '@/components/legal/AgreementToast';
