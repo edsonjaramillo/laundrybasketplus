09b1196fa0e5950aabeec4bf845186f595a1d22c
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-18 12:23:18 -0500

    added graphcms functionality

---
+++ lib/graphcms/queries.ts
@@ -0,0 +1,155 @@
+import { gql } from 'graphql-request';
+
+export const getHomepageProps = gql`
+  query getHomepageProps {
+    stores(orderBy: sort_ASC) {
+      id
+      name
+      description
+      slug
+      image {
+        id
+        url(transformation: { image: { resize: { fit: clip, width: 800 } } })
+        alt
+      }
+      hasExternalPage
+      externalUrl
+    }
+    callToAction(where: { id: "cl3ak154l759w0bk747xcq2s3" }) {
+      id
+      header
+      subheader
+    }
+    aboutus(where: { id: "cl3aktuhi7tat0bk7w4fsvbcn" }) {
+      id
+      header
+      subheader
+    }
+  }
+`;
+
+export const getStores = gql`
+  query getStores {
+    stores {
+      id
+      name
+      description
+      slug
+      image {
+        id
+        url(transformation: { image: { resize: { fit: clip, width: 800 } } })
+        alt
+      }
+      address {
+        id
+        street
+        city
+        state
+        zipcode
+      }
+      article {
+        raw
+      }
+      hasExternalPage
+      externalUrl
+    }
+  }
+`;
+
+export const getStore = gql`
+  query getStore($slug: String!) {
+    store(where: { slug: $slug }) {
+      id
+      name
+      description
+      slug
+      image {
+        id
+        url(transformation: { image: { resize: { fit: clip, width: 800 } } })
+        width
+        height
+        alt
+      }
+      address {
+        id
+        street
+        city
+        state
+        zipcode
+      }
+      article {
+        raw
+      }
+      hasExternalPage
+      externalUrl
+    }
+  }
+`;
+
+export const getJobListings = gql`
+  query getJobListings {
+    joblistings {
+      id
+      title
+      description
+      datePosted
+      isFulltime
+      wage
+      estimatedHours
+      store {
+        id
+        name
+        address {
+          id
+          street
+          city
+          state
+          zipcode
+        }
+      }
+    }
+  }
+`;
+
+export const getJobListing = gql`
+  query getJobListing($id: ID!) {
+    joblisting(where: { id: $id }) {
+      id
+      title
+      description
+      datePosted
+      isFulltime
+      wage
+      estimatedHours
+      article {
+        raw
+      }
+      store {
+        id
+        name
+        description
+        address {
+          id
+          street
+          city
+          state
+          zipcode
+        }
+      }
+    }
+  }
+`;
+
+export const getSitemapLinks = gql`
+  query getSitemapLinks {
+    stores(orderBy: sort_ASC) {
+      id
+      name
+      slug
+    }
+    joblistings(orderBy: datePosted_ASC) {
+      id
+      title
+    }
+  }
+`;
---
+++ lib/graphcms/types.ts
@@ -0,0 +1,55 @@
+export interface StoreType {
+  id: number;
+  name: string;
+  description: string;
+  image: ImageType;
+  slug: string;
+  address: AddressType;
+  article: RichTextType;
+  hasExternalPage: boolean;
+  externalUrl: string;
+}
+
+export interface AddressType {
+  id: string;
+  street: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
+export interface ImageType {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+  alt: string;
+}
+
+export interface JobListingType {
+  id: string;
+  datePosted: string;
+  title: string;
+  description: string;
+  isFulltime: boolean;
+  wage: number;
+  estimatedHours: number;
+  article: RichTextType;
+  store: StoreType;
+}
+
+interface RichTextType {
+  raw: any[];
+}
+
+export interface CTAType {
+  id: string;
+  header: string;
+  subheader: string;
+}
+
+export interface AboutUsType {
+  id: string;
+  header: string;
+  subheader: string;
+}
