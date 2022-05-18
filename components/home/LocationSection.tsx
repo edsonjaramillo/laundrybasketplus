151e831c886942ea4d8fd1406501b4526fdc5a01
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-18 12:42:05 -0500

    added location section to homepage

---
+++ components/home/LocationSection.tsx
@@ -0,0 +1,21 @@
+import { Location } from '@/components/index';
+import { StoreType } from '@/lib/graphcms/types';
+
+interface StoreSectionProps {
+  stores: StoreType[];
+}
+
+const StoresSection = ({ stores }: StoreSectionProps) => (
+  <section className='locationsection' id='locations'>
+    <h2 className='locationsection__header responsive-width'>
+      Laundry Basket +
+      <br />
+      Locations
+    </h2>
+    {stores.map((store, i) => (
+      <Location key={store.id} store={store} isReverse={i % 2 === 0} />
+    ))}
+  </section>
+);
+
+export default StoresSection;
